9354c04645e1cd8e370a4ec45030df0b38af0eeb
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-05-05 17:19:50 -0300

    Seed genre, movie, review

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,11 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_genre (name) VALUES ('Fantasia');
+
+INSERT INTO tb_movie (title,sub_title,year, img_url, synopsis,genre_id) VALUES ('Star Wars', 'Retorno de Jedi', 1983, 'https://www.unimado.pt/images/covers/star-wars-episodio-vi-o-regresso-do-jedi.webp', 'A aliança rebelde irá dar um fim ao império', 1);
+
+INSERT INTO tb_review (text, user_id, movie_id) VALUES ('Filme incrível', 1, 1);
+
+
